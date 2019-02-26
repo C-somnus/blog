c50385b9152997a05e14a8fb2d1b26c57099008f
Somnus <970859212@qq.com>
2019-02-26 15:26:11 +0800

    Update 原型及原型链.js

--- 原型及原型链.js
+++ 原型及原型链.js
@@ -6,3 +6,6 @@ Function.prototype.__proto__ === Object.prototype
 Function.__proto__ === Function.prototype
 
 // prototype是函数的一个对象（即原型）在函数创建的时候产生 一开始只有一个属性constructor 值为自身的构造函数
+
+// 任何对象的__proto__ 都是指向 Object.prototype
+// 任何函数的__proto__ 都指向 Function.prototype
